80c24a0af49903ba4873778a2631d9986cf55184
vasder <vinithktp@gmail.com>
2017-12-12 00:50:37 -0500

    InitialCommit
    InitialCommit

---
+++ MaterialApp/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ContentsComponent } from './components/contents/contents.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenubarComponent } from './components/menubar/menubar.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ContentsComponent,
+    FooterComponent,
+    MenubarComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
