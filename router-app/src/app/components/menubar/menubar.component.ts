311fe12bf33a06ced8c3ced459f6b239ee34961d
vasder <vinithktp@gmail.com>
2018-02-19 22:12:22 -0500

    Fixed security vulnerability
    Fixed security vulnerability

--- router-app/src/app/components/menubar/menubar.component.ts
+++ router-app/src/app/components/menubar/menubar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class MenubarComponent implements OnInit {
 
   uname = sessionStorage.Username;
-  if(uname === '') {
+  if(uname) {
     console.log('Active session exists');
   }
   constructor() { }
